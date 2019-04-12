3883d46c92688ea1842fe8496718a6d88271920b
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 05:09:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(array)
   end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each_upcase do |elem|
+    puts "#{elem}!"
 end
 
 def long_planeteer_calls# code an argument here
