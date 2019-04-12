de4cc1dbeb87a4a9c8b655aead0e436ea20451dd
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 05:12:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array = []
   array.collect do |call|
-    
+    call.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
