2333bfddbdfb6b3d12cf207e17694b6e23bd0571
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 05:19:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,15 +10,7 @@ def summon_captain_planet(array)
   end 
 end
 
-def long_planeteer_calls(array)
-  if calls_long.any do |word|
-    word.length > 4
-    end 
-  return "true"
-  else 
-  return "false"
-  end 
-end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
