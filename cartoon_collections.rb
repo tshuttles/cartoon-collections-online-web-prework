7061a999410950323bdf0cdb5345fde3d155bc1a
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-12 05:19:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   array.any? do |word|
     word.length > 4
   end  
-end	end
+end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
